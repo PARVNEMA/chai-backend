c39627b58d7d45afb08ce49c6297498329b49449
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-09-04 10:16:28 +0530

    video controller done

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,47 +1,192 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary, deleteCloudinary } from "../utils/cloudinary.js";
+import fs from "fs";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-})
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  //TODO: get all videos based on query, sort, pagination
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
-})
+  const { title, description } = req.body;
+  // TODO: get video, upload to cloudinary, create video
+  if (!title || !description) {
+    throw new ApiError(400, "title and description required");
+  }
+  if (!req.files.videoFile || !req.files.thumbnail) {
+    if (req.files.videoFile) {
+      fs.unlinkSync(req.files?.videoFile[0]?.path);
+    }
+    if (req.files.thumbnail) {
+      fs.unlinkSync(req.files?.thumbnail[0]?.path);
+    }
+    throw new ApiError(401, "either videoFile or thumbnail is missing");
+  }
+  const videoPath = req.files?.videoFile[0]?.path;
+  const thumbnailPath = req.files?.thumbnail[0]?.path;
+
+  if (!videoPath || !thumbnailPath) {
+    throw new ApiError(400, "Please upload video and thumbnail");
+  }
+
+  const videoFile = await uploadOnCloudinary(videoPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailPath);
+
+  if (!videoFile || !thumbnail) {
+    throw new ApiError(
+      400,
+      "Something went wrong while uploading video and thumbnail"
+    );
+  }
+
+  const vid = await Video.create({
+    title,
+    description,
+    videoFile: videoFile.secure_url,
+    thumbnail: thumbnail.secure_url,
+    duration: videoFile?.duration,
+  });
+
+  console.log("Video", vid);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, vid, "Video created successfully"));
+});
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Video not found");
+  }
+
+  const video = await Video.findOne({ _id: videoId });
+  // OR  const video = await Video.findOne({ _id: mongoose.Types.ObjectId(videoId) });
+  if (!video) {
+    throw new ApiError(400, "Video not found");
+  }
+
+  return res.status(200).json(new ApiResponse(200, video, "Video found"));
+  //TODO: get video by id
+});
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
+  const { videoId } = req.params;
+  const { title, description } = req.body;
+  if (!title || !description) {
+    throw new ApiError(400, "title and description required");
+  }
+  const thumbnailPath = req.file?.path;
 
-})
+  if (!thumbnailPath) {
+    throw new ApiError(400, "Please upload  thumbnail");
+  }
+  if (!videoId) {
+    throw new ApiError(400, "Video not found");
+  }
+
+  const thumbnail = await uploadOnCloudinary(thumbnailPath);
+
+  if (!thumbnail.url) {
+    throw new ApiError(400, "Error while uploading on thumbnail");
+  }
+
+  const updatedVideo = await Video.findByIdAndUpdate(
+    new mongoose.Types.ObjectId(videoId),
+    {
+      $set: {
+        title,
+        description: description,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  if (!updatedVideo) {
+    throw new ApiError(400, "Video not found");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { updatedVideo },
+        "video details updated successfully"
+      )
+    );
+
+  //TODO: update video details like title, description, thumbnail
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
-})
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Video Id not found");
+  }
+
+  const video = await Video.findOneAndDelete({
+    _id: new mongoose.Types.ObjectId(videoId),
+  });
+  if (!video) {
+    throw new ApiError(400, "Video not found");
+  }
+
+  const clouddelete = await deleteCloudinary(video.videoFile);
+  
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "video deleted  successfully"));
+
+  //TODO: delete video
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Video not found");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(400, "Video not found");
+  }
+  const updateduser = await Video.findByIdAndUpdate(
+    new mongoose.Types.ObjectId(videoId),
+    {
+      $set: {
+        isPublished: video.isPublished ? false : true,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updateduser,
+        "video toggle published updated successfully"
+      )
+    );
+});
 
 export {
-    getAllVideos,
-    publishAVideo,
-    getVideoById,
-    updateVideo,
-    deleteVideo,
-    togglePublishStatus
-}
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,42 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { ApiError } from "./ApiError.js";
+import { ApiResponse } from "./ApiResponse.js";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
-    }
-}
+const deleteCloudinary = async (itemId) => {
+  try {
+    await cloudinary.uploader.destroy(itemId, function (result) {
+      throw new ApiResponse(200, "deleted on cloudinary");
+    });
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while deleting on cloudinary"
+    );
+  }
+};
 
-
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinary, deleteCloudinary };
