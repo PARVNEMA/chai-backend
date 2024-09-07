4a0d1b90e820f413c0f8699ca22d79e5003dfa6f
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-09-07 19:33:39 +0530

    likes controller done

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,132 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+  const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "video not found");
+  }
+
+  const alreadylikedornot = await Like.findOne({
+    video: new mongoose.Types.ObjectId(videoId),
+    likedBy: new mongoose.Types.ObjectId(req.user?._id),
+  });
+  if (alreadylikedornot) {
+    const deletelike = await Like.findOneAndDelete({
+      video: new mongoose.Types.ObjectId(videoId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, deletelike, "unliked successfully"));
+  } else {
+    const like = await Like.create({
+      video: new mongoose.Types.ObjectId(videoId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, like, "liked successfully"));
+  }
+
+  //TODO: toggle like on video
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  const { commentId } = req.params;
 
-})
+  const alreadylikedornot = await Like.findOne({
+    comment: new mongoose.Types.ObjectId(commentId),
+    likedBy: new mongoose.Types.ObjectId(req.user?._id),
+  });
+  if (alreadylikedornot) {
+    const deletelike = await Like.findOneAndDelete({
+      comment: new mongoose.Types.ObjectId(commentId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, deletelike, "unliked successfully"));
+  } else {
+    const like = await Like.create({
+      comment: new mongoose.Types.ObjectId(commentId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, like, "liked successfully"));
+  }
+  //TODO: toggle like on comment
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  const { tweetId } = req.params;
+
+  const alreadylikedornot = await Like.findOne({
+    tweet: new mongoose.Types.ObjectId(tweetId),
+    likedBy: new mongoose.Types.ObjectId(req.user?._id),
+  });
+  if (alreadylikedornot) {
+    const deletelike = await Like.findOneAndDelete({
+      tweet: new mongoose.Types.ObjectId(tweetId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, deletelike, "unliked successfully"));
+  } else {
+    const like = await Like.create({
+      tweet: new mongoose.Types.ObjectId(tweetId),
+      likedBy: new mongoose.Types.ObjectId(req.user?._id),
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, like, "liked successfully"));
+  }
+  //TODO: toggle like on tweet
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+  //TODO: get all liked videos
+  const likedVideos = await Like.aggregate([
+    {
+      $match: {
+        likedBy: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "liked",
+      },
+    },
+    {
+      $addFields: {
+        TotalLikes: {
+          $size: "$liked",
+        },
+      },
+    },
+    {
+      $project: {
+        likedBy: 1,
+        TotalLikes: 1,
+        liked: 1,
+      },
+    },
+  ]);
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+  return res
+    .status(200)
+    .json(new ApiResponse(200, likedVideos, "liked videos"));
+});
+
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
